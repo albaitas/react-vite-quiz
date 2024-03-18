3a3dd37866fdc21793c5b063541fdfe0350f8164
Albaitas <internet622@gmail.com>
2024-03-18 12:29:12 +0200

    feat: add audio melodies

---
+++ src/utils/playSound.jsx
@@ -0,0 +1,16 @@
+const playSound = (sound) => {
+  const audio = new Audio(`/sounds/${sound}`);
+  audio.play();
+};
+
+export const playCorrectAnswer = () => {
+  playSound('correct-answer.mp3');
+};
+
+export const playWrongAnswer = () => {
+  playSound('wrong-answer.mp3');
+};
+
+export const playQuizEnd = () => {
+  playSound('quiz-end.mp3');
+};
