6725b5ad16ed63c5035b3ee042edc4808c9d1afe
Albaitas <internet622@gmail.com>
2024-03-18 16:06:06 +0200

    feat: display result if questions are over

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,8 +11,14 @@ function Home() {
   const [selectedOption, setSelectedOption] = useState(null);
   const [timerActive, setTimerActive] = useState(true);
   const [locked, setLocked] = useState(true);
+  const [result, setResult] = useState(false);
 
   const next = useCallback(() => {
+    if (index === questionsData.length - 1) {
+      playQuizEnd();
+      setResult(true);
+      return 0;
+    }
     setLock(true);
     setLocked(true);
     setSelectedOption(null);
@@ -37,32 +43,40 @@ function Home() {
   };
 
   return (
-    <>
-      <h1>Quiz App</h1>
-      <h3 className='blue'>
-        {index + 1}. {data.question}
-      </h3>
-      <code>{data.example}</code>
+    <div className='container'>
+      {result ? (
+        <div className='result'>
+          <h1 className='multicolortext'>Congratulations!!!</h1>
+        </div>
+      ) : (
+        <>
+          <h1>Quiz App</h1>
 
-      <ul>
-        {data.options.map((option, i) => (
-          <li
-            key={i}
-            className={classNames({
-              correct: i === data.ans && selectedOption !== null,
-              wrong: i === selectedOption && i !== data.ans
-            })}
-            onClick={() => handleOptionClick(i)}
-          >
-            {option}
-          </li>
-        ))}
-      </ul>
-      <button disabled={locked} onClick={next}>
-        Next
-      </button>
-    </>
+          <h3 className='blue'>
+            {index + 1}. {data.question}
+          </h3>
+          <code>{data.example}</code>
+
+          <ul>
+            {data.options.map((option, i) => (
+              <li
+                key={i}
+                className={classNames({
+                  correct: i === data.ans && selectedOption !== null,
+                  wrong: i === selectedOption && i !== data.ans
+                })}
+                onClick={() => handleOptionClick(i)}
+              >
+                {option}
+              </li>
+            ))}
+          </ul>
+          <button disabled={locked} onClick={next}>
+            Next
+          </button>
+        </>
+      )}
+    </div>
   );
 }
-
 export default Home;
