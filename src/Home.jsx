b4a6e85b2270e714c8a67ea4c8df15399fba86ba
Albaitas <internet622@gmail.com>
2024-03-18 12:09:15 +0200

    feat: display quiz answers with map()

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,17 @@ import classNames from 'classnames';
 function Home() {
   const [index, setIndex] = useState(0);
   const [data, setData] = useState(questionsData[index]);
-  return <></>;
+  return (
+    <>
+      <h1>Quiz App</h1>
+
+      <ul>
+        {data.options.map((option, i) => (
+          <li key={i}>{option}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default Home;
