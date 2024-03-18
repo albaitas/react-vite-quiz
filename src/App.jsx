3525fdaea0ddcfc10dc3b5164509208ed5d2c4f1
Albaitas <internet622@gmail.com>
2024-03-18 16:28:33 +0200

    display Intro or Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
+import { useState } from 'react';
 import Home from './Home';
 import Intro from './Intro';
 
 function App() {
-  return (
-    <>
-      <Intro />
-      <Home />
-    </>
-  );
-}
+  const [show, setShow] = useState(false);
 
+  const onClick = () => {
+    setShow(true);
+  };
+
+  return <>{show ? <Home /> : <Intro onClick={onClick} />}</>;
+}
 export default App;
